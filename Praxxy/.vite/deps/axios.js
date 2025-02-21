9e5074ff206defbd92ac60365994da36a69a0a52
Rhuzzel Paramio <87302705+CodeDecoder13@users.noreply.github.com>
2025-02-21 21:01:17 +0800

    ss

---
+++ Praxxy/.vite/deps/axios.js
@@ -0,0 +1,39 @@
+import {
+  Axios,
+  AxiosError,
+  AxiosHeaders,
+  Cancel,
+  CancelToken,
+  CanceledError,
+  HttpStatusCode,
+  VERSION,
+  all,
+  axios_default,
+  formToJSON,
+  getAdapter,
+  isAxiosError,
+  isCancel,
+  mergeConfig,
+  spread,
+  toFormData
+} from "./chunk-64C7XPYY.js";
+import "./chunk-EWTE5DHJ.js";
+export {
+  Axios,
+  AxiosError,
+  AxiosHeaders,
+  Cancel,
+  CancelToken,
+  CanceledError,
+  HttpStatusCode,
+  VERSION,
+  all,
+  axios_default as default,
+  formToJSON,
+  getAdapter,
+  isAxiosError,
+  isCancel,
+  mergeConfig,
+  spread,
+  toFormData
+};
